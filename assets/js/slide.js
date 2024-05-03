6de7429e0e4532ffd5d8e5d401df698fa6dee4e4
PongSuwapat <78334470+PongSuwapat@users.noreply.github.com>
2024-05-03 21:43:22 +0700

    auto slide

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,26 +1,35 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
 
+autoSlides();
+
+
+function autoSlides()
+{
+    slideIndex++;
+    showSlides(slideIndex);
+    setTimeout(autoSlides,5000);
+}
+// Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex);
+  }
+  
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+  slides[slideIndex-1].style.display = "block";
+
+
 }
